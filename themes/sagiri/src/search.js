81d2882599d357b39c8e96367d53fd363e8cc5cb
hxf <570195314@qq.com>
2021-09-28 15:21:06 +0800

    fix(update): 修改本地搜索

--- themes/sagiri/src/search.js
+++ themes/sagiri/src/search.js
@@ -1,5 +1,7 @@
-$('.search-form').submit(function (e) {
-  e.preventDefault();
-  window.open(`https://www.google.com/search?&q=site%3A${window.location.host}+${encodeURIComponent($('.search-form input[name="keyword"]').val())}`);
-});
+// $('.search-form').submit(function (e) {
+//   e.preventDefault();
+//   window.open(`https://www.google.com/search?&q=site%3A${window.location.host}+${encodeURIComponent($('.search-form input[name="keyword"]').val())}`);
+// });
 
+// var path = "/search.xml";
+// searchFunc(path, 'local-search-input', 'local-search-result');
