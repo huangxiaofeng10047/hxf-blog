54c3741f8307cbead95853ca61ef7bf82d30f82a
hxf <570195314@qq.com>
2021-09-28 14:59:26 +0800

    fix(update): fix 本地搜索

--- themes/sagiri/src/search.js
+++ themes/sagiri/src/search.js
@@ -2,3 +2,4 @@ $('.search-form').submit(function (e) {
   e.preventDefault();
   window.open(`https://www.google.com/search?&q=site%3A${window.location.host}+${encodeURIComponent($('.search-form input[name="keyword"]').val())}`);
 });
+
